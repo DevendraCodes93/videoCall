a591c36ccdac762480c7d8d5c37f3fe598a26653
DevendraCodes93 <devu935352@gmail.com>
2025-05-19 22:34:27 +0530

    improved

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import { app, server } from "./lib/socket.js";
 dbConnect();
 app.use(
   cors({
-    origin: " https://frontend-video-call-app.vercel.app",
+    origin: "https://frontend-video-call-app-uc65.vercel.app",
     credentials: true,
   })
 );
@@ -17,6 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/user", userRouter);
-server.listen(3000, () => {
-  console.log("server running on port 3000");
+server.listen(3001, "0.0.0.0", () => {
+  console.log("server running on port 3001");
 });
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,8 +1,12 @@
 import jwt from "jsonwebtoken";
 
 export const authMiddleware = async (req, res, next) => {
- 
-  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  let token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  // Remove quotes if present
+  if (token?.startsWith('"') && token?.endsWith('"')) {
+    token = token.slice(1, -1);
+  }
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized", success: false });
